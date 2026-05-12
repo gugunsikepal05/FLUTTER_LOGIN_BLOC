223f9265824179c2501a3d51d059770465caf1bc
BosFiki2005 <fiki061005@gmail.com>
2026-05-12 17:44:29 +0700

    fiki ganteng
    menambah login

---
+++ lib/login/login.dart
@@ -0,0 +1,11 @@
+library login;
+
+export 'bloc/login_bloc.dart';
+
+export 'models/models.dart';
+export 'models/password.dart';
+export 'models/username.dart';
+
+export 'view/login_form.dart';
+export 'view/login_page.dart';
+export 'view/view.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_login/app.dart';
+import 'package:flutter_login_bloc/app.dart';
 
 void main() => runApp(const App());
